e0312c5d9b5651f06edd5c01a1f598a302123127
pro-soichiro <dr.1987626@gmail.com>
2022-06-29 18:49:39 +0900

    seedとrootの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+2.times do |n|
+  User.create(name: "user#{n+1}")
+end
+2.times do |n|
+  User.create(name: "user#{n+3}",scout_email_notification: false)
+end
+2.times do |n|
+  User.create(name: "user#{n+5}",introduction_project_email: false)
+end
+2.times do |n|
+  User.create(name: "user#{n+7}",advertisement_delivery: false)
+end
+2.times do |n|
+  User.create(name: "user#{n+9}",email_magazine: false)
+end
+2.times do |n|
+  User.create(name: "user#{n+11}",contact_from_manager: false)
+end
