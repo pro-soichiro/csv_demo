428ffcacc5e37f2b0e69010463b613d0dd9f33ac
pro-soichiro <dr.1987626@gmail.com>
2022-06-30 08:03:26 +0900

    CSV出力内容の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,19 @@ class UsersController < ApplicationController
 
   def send_users_csv(users)
     csv_data = CSV.generate do |csv|
-      header = %w(名前 スカウト 案件紹介 広告配信 メルマガ 運営連絡)
+      header = %w(ID 名前 スカウト 案件紹介 広告配信 メルマガ 運営連絡 作成日時 更新日時)
       csv << header
       users.each do |user|
         values = [
+          user.id
           user.name,
           user.scout_email_notification == true ? '受け取る' : 'NG',
           user.introduction_project_email == true ? '受け取る' : 'NG',
           user.advertisement_delivery == true ? '受け取る' : 'NG',
           user.email_magazine == true ? '受け取る' : 'NG',
           user.contact_from_manager == true ? '受け取る' : 'NG',
+          user.created_at,
+          user.updated_at
         ]
         csv << values
       end
